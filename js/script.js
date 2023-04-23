ef0c9c28b36018151e8d5ef9ee8b0664286f2892
Rostislav Sergeev <113761086+itrostik@users.noreply.github.com>
2023-04-23 09:18:30 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+});
